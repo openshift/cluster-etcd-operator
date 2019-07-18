a9ff9abd5275a37f161bc842ef49786e5d87e283
Sam Batschelet <sbatsche@redhat.com>
2019-07-17 22:20:41 -0400

    cleanup: starter
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -6,20 +6,15 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/informers"
-	"k8s.io/client-go/informers/internalinterfaces"
 	"k8s.io/client-go/kubernetes"
 
 	configv1 "github.com/openshift/api/config/v1"
 	configv1client "github.com/openshift/client-go/config/clientset/versioned"
 	configv1informers "github.com/openshift/client-go/config/informers/externalversions"
-	etcdv1client "github.com/openshift/client-go/etcd/clientset/versioned"
-	etcdv1informers "github.com/openshift/client-go/etcd/informers/externalversions"
 	operatorversionedclient "github.com/openshift/client-go/operator/clientset/versioned"
 	operatorv1informers "github.com/openshift/client-go/operator/informers/externalversions"
 
 	"github.com/openshift/library-go/pkg/controller/controllercmd"
-	"github.com/openshift/library-go/pkg/operator/staticpod/controller/revision"
 	"github.com/openshift/library-go/pkg/operator/status"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
 
@@ -40,10 +35,6 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 		return err
 	}
 	configClient, err := configv1client.NewForConfig(ctx.KubeConfig)
-	if err != nil {
-		return err
-	}
-	etcdClient, err := etcdv1client.NewForConfig(ctx.KubeConfig)
 	if err != nil {
 		return err
 	}
@@ -113,21 +104,10 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 		ctx.EventRecorder,
 	)
 
-	var tweakListOptions internalinterfaces.TweakListOptionsFunc
-	factory := informers.NewFilteredSharedInformerFactory(clientset, 0, "openshift-etcd", tweakListOptions)
-	podInformer := factory.Core().V1().Pods()
-
-	etcdInformers := etcdv1informers.NewSharedInformerFactory(etcdClient, 0)
-
 	coreClient := clientset
 
 	clusterMemberController := clustermembercontroller.NewClusterMemberController(
 		coreClient,
-		podInformer,
-
-		etcdClient.EtcdV1().ClusterMemberRequests(),
-		etcdInformers,
-
 		operatorClient,
 
 		kubeInformersForNamespaces.InformersFor("openshift-etcd"),
@@ -136,7 +116,6 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 	operatorConfigInformers.Start(ctx.Done())
 	kubeInformersForNamespaces.Start(ctx.Done())
 	configInformers.Start(ctx.Done())
-	etcdInformers.Start(ctx.Done())
 
 	go resourceSyncController.Run(1, ctx.Done())
 	go configObserver.Run(1, ctx.Done())
@@ -145,55 +124,4 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 
 	<-ctx.Done()
 	return fmt.Errorf("stopped")
-}
-
-// RevisionConfigMaps is a list of configmaps that are directly copied for the current values.  A different actor/controller modifies these.
-// the first element should be the configmap that contains the static pod manifest
-var RevisionConfigMaps = []revision.RevisionResource{
-	{Name: "kube-apiserver-pod"},
-
-	{Name: "config"},
-	{Name: "kube-apiserver-cert-syncer-kubeconfig"},
-	{Name: "oauth-metadata", Optional: true},
-	{Name: "cloud-config", Optional: true},
-
-	// these need to removed, but if we remove them now, the cluster will die because we don't reload them yet
-	{Name: "etcd-serving-ca"},
-	{Name: "kube-apiserver-server-ca", Optional: true},
-	{Name: "kubelet-serving-ca"},
-	{Name: "sa-token-signing-certs"},
-}
-
-// RevisionSecrets is a list of secrets that are directly copied for the current values.  A different actor/controller modifies these.
-var RevisionSecrets = []revision.RevisionResource{
-	// these need to removed, but if we remove them now, the cluster will die because we don't reload them yet
-	{Name: "etcd-client"},
-	// this is needed so that the cert syncer itself can request certs.  It uses localhost
-	{Name: "kube-apiserver-cert-syncer-client-cert-key"},
-	{Name: "kubelet-client"},
-}
-
-var CertConfigMaps = []revision.RevisionResource{
-	{Name: "aggregator-client-ca"},
-	{Name: "client-ca"},
-}
-
-var CertSecrets = []revision.RevisionResource{
-	{Name: "aggregator-client"},
-	{Name: "localhost-serving-cert-certkey"},
-	{Name: "service-network-serving-certkey"},
-	{Name: "external-loadbalancer-serving-certkey"},
-	{Name: "internal-loadbalancer-serving-certkey"},
-
-	{Name: "user-serving-cert", Optional: true},
-	{Name: "user-serving-cert-000", Optional: true},
-	{Name: "user-serving-cert-001", Optional: true},
-	{Name: "user-serving-cert-002", Optional: true},
-	{Name: "user-serving-cert-003", Optional: true},
-	{Name: "user-serving-cert-004", Optional: true},
-	{Name: "user-serving-cert-005", Optional: true},
-	{Name: "user-serving-cert-006", Optional: true},
-	{Name: "user-serving-cert-007", Optional: true},
-	{Name: "user-serving-cert-008", Optional: true},
-	{Name: "user-serving-cert-009", Optional: true},
 }
