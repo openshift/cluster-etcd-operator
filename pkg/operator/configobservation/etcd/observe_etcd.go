008bc8525bb6a09fffa6860a9bd657f6a5acf234
Alay Patel <alay1431@gmail.com>
2019-09-25 16:51:21 -0400

    pkg/operator: fix ObservePendingClusterMembers

--- pkg/operator/configobservation/etcd/observe_etcd.go
+++ pkg/operator/configobservation/etcd/observe_etcd.go
@@ -146,11 +146,6 @@ func ObservePendingClusterMembers(genericListers configobserver.Listers, recorde
 	if err != nil {
 		errs = append(errs, err)
 	}
-	if found {
-		if err := unstructured.SetNestedSlice(observedConfig, currentClusterMembers, clusterMemberPath...); err != nil {
-			errs = append(errs, err)
-		}
-	}
 
 	var etcdURLs []interface{}
 	etcdEndpoints, err := listers.OpenshiftEtcdEndpointsLister.Endpoints(etcdEndpointNamespace).Get(etcdEndpointName)
@@ -187,6 +182,11 @@ func ObservePendingClusterMembers(genericListers configobserver.Listers, recorde
 	}
 
 	if len(errs) > 0 {
+		if found {
+			if err := unstructured.SetNestedSlice(observedConfig, currentClusterMembers, clusterMemberPath...); err != nil {
+				errs = append(errs, err)
+			}
+		}
 		return
 	}
 
