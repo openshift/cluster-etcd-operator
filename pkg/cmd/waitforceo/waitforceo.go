a22c4aca141830301b5b165b78fe0ad67a5e2bf3
Alay Patel <alay1431@gmail.com>
2019-11-26 16:03:09 -0500

    *: add wait-for-ceo sub-command

---
+++ pkg/cmd/waitforceo/waitforceo.go
@@ -0,0 +1,59 @@
+package waitforceo
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/openshift/cluster-etcd-operator/pkg/operator/bootstrapteardown"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+)
+
+type waitForCeoOpts struct {
+	errOut     io.Writer
+	kubeconfig string
+}
+
+// NewWaitForCeoCommand waits for cluster-etcd-operator to bootstrap.
+func NewWaitForCeoCommand(errOut io.Writer) *cobra.Command {
+	waitForCeoOpts := &waitForCeoOpts{
+		errOut: errOut,
+	}
+	cmd := &cobra.Command{
+		Use:   "wait-for-ceo --FLAGS",
+		Short: "waits for cluster-etcd-operator to finish bootstrap etcd cluster",
+		Long:  "This command makes sure that the cluster-etcd-operator is available before signaling bootstrap is done",
+		Run: func(cmd *cobra.Command, args []string) {
+			must := func(fn func() error) {
+				if err := fn(); err != nil {
+					if cmd.HasParent() {
+						klog.Fatal(err)
+						fmt.Fprint(waitForCeoOpts.errOut, err.Error())
+					}
+				}
+			}
+			must(waitForCeoOpts.Run)
+		},
+	}
+
+	waitForCeoOpts.AddFlags(cmd.Flags())
+	return cmd
+}
+
+func (w *waitForCeoOpts) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&w.kubeconfig, "kubeconfig", "", "kubeconfig for the cluster it is bootstrapping")
+}
+
+func (w *waitForCeoOpts) Run() error {
+	config, err := clientcmd.BuildConfigFromFlags("", w.kubeconfig)
+	if err != nil {
+		klog.Errorf("error loading kubeconfig: %#v", err)
+	}
+	if err := bootstrapteardown.WaitForEtcdBootstrap(context.TODO(), config); err != nil {
+		klog.Errorf("error waiting for cluster-etcd-operator %#v", err)
+	}
+	return nil
+}
---
+++ pkg/operator/bootstrapteardown/waitforceo.go
@@ -0,0 +1,75 @@
+package bootstrapteardown
+
+import (
+	"context"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	operatorversionedclient "github.com/openshift/client-go/operator/clientset/versioned"
+	operatorv1helpers "github.com/openshift/library-go/pkg/operator/v1helpers"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	clientwatch "k8s.io/client-go/tools/watch"
+	"k8s.io/klog"
+)
+
+func WaitForEtcdBootstrap(ctx context.Context, config *rest.Config) error {
+	operatorConfigClient, err := operatorversionedclient.NewForConfig(config)
+	if err != nil {
+		klog.Errorf("error getting operator client config: %#v", err)
+		return err
+	}
+
+	// TODO: figure out if we can timeout after 30 mins by
+	// passing a different context here
+	if err := waitForEtcdBootstrap(ctx, operatorConfigClient.OperatorV1().RESTClient()); err != nil {
+		klog.Errorf("error watching etcd: %#v", err)
+	}
+	return err
+}
+
+func waitForEtcdBootstrap(ctx context.Context, operatorRestClient rest.Interface) error {
+	_, err := clientwatch.UntilWithSync(
+		ctx,
+		cache.NewListWatchFromClient(operatorRestClient, "etcds", "", fields.OneTermEqualSelector("metadata.name", "cluster")),
+		&operatorv1.Etcd{},
+		nil,
+		func(event watch.Event) (bool, error) {
+			switch event.Type {
+			case watch.Added, watch.Modified:
+				etcd, ok := event.Object.(*operatorv1.Etcd)
+				if !ok {
+					klog.Warningf("Expected an Etcd object but got a %q object instead", event.Object.GetObjectKind().GroupVersionKind())
+					return false, nil
+				}
+				return done(etcd)
+			}
+			klog.Infof("Still waiting for the cluster-etcd-operator to bootstrap...")
+			return false, nil
+		},
+	)
+
+	if err != nil {
+		klog.Errorf("error waiting for etcd CR: %#v", err)
+		return err
+	}
+
+	klog.Infof("cluster-etcd-operator bootstrap etcd")
+	return nil
+}
+
+func done(etcd *operatorv1.Etcd) (bool, error) {
+	if etcd.Spec.ManagementState == operatorv1.Unmanaged {
+		klog.Info("Cluster etcd operator is in Unmanaged mode")
+		return true, nil
+	}
+	if operatorv1helpers.IsOperatorConditionTrue(etcd.Status.Conditions, operatorv1.OperatorStatusTypeAvailable) &&
+		operatorv1helpers.IsOperatorConditionFalse(etcd.Status.Conditions, operatorv1.OperatorStatusTypeProgressing) &&
+		operatorv1helpers.IsOperatorConditionFalse(etcd.Status.Conditions, operatorv1.OperatorStatusTypeDegraded) {
+		klog.Info("Cluster etcd operator bootstrapped successfully")
+		return true, nil
+	}
+	klog.Infof("Still waiting for the cluster-etcd-operator to bootstrap")
+	return false, nil
+}
