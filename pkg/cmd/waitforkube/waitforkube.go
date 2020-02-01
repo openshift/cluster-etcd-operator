a78bb3335c881eb553f12f8d1656e52679eb250e
Sam Batschelet <sbatsche@redhat.com>
2020-02-01 10:21:35 -0500

    pkg/cmd/waitforkube: don't wait if etcd has previously been initialized as member
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- pkg/cmd/waitforkube/waitforkube.go
+++ pkg/cmd/waitforkube/waitforkube.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -14,15 +15,17 @@ import (
 const (
 	retryDuration = 10 * time.Second
 	saTokenPath   = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	dataDir       = "/var/lib/etcd" //TODO should be flag or ENV
+	dbPath        = "member/snap/db"
 )
 
-type waitForKubeOpts struct {
+type waitForKube struct {
 	errOut io.Writer
 }
 
 // NewWaitForKubeCommand waits for kube to come up before continuing to start the rest of the containers.
 func NewWaitForKubeCommand(errOut io.Writer) *cobra.Command {
-	waitForKubeOpts := &waitForKubeOpts{
+	waitForKube := &waitForKube{
 		errOut: errOut,
 	}
 	cmd := &cobra.Command{
@@ -34,29 +37,51 @@ func NewWaitForKubeCommand(errOut io.Writer) *cobra.Command {
 				if err := fn(); err != nil {
 					if cmd.HasParent() {
 						klog.Fatal(err)
-						fmt.Fprint(waitForKubeOpts.errOut, err.Error())
+						fmt.Fprint(waitForKube.errOut, err.Error())
 					}
 				}
 			}
-			must(waitForKubeOpts.Run)
+			must(waitForKube.Run)
 		},
 	}
 
 	return cmd
 }
 
-func (w *waitForKubeOpts) Run() error {
+func (w *waitForKube) Run() error {
+	dataFilePath := filepath.Join(dataDir, dbPath)
+	if CheckEtcdDataFileExists(dataFilePath) {
+		klog.Infof("etcd has previously been initialized as a member")
+		return nil
+	}
 	wait.PollInfinite(retryDuration, func() (bool, error) {
 		if _, err := os.Stat(saTokenPath); os.IsNotExist(err) {
 			klog.Infof("waiting for kube service account resources to sync: %v", err)
 			return false, nil
 		}
+		klog.Infof("kube dependencies verified, service account resources found")
 		return true, nil
 	})
 	if !inCluster() {
-		return fmt.Errorf("kube env not populated")
+		return fmt.Errorf("container missing required env KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT")
 	}
 	return nil
+}
+
+// CheckEtcdDataFileExists checks for the existance of an etcd db file. If the db exists we can assume this etcd
+// has previously started or is currently being recovered. If true we can not assume kube exists yet or will
+// during the life of this container. For example disaster recovery.
+// TODO organize into a general util
+func CheckEtcdDataFileExists(dataFilePath string) bool {
+	if _, err := os.Stat(dataFilePath); err != nil {
+		if os.IsNotExist(err) {
+			klog.Infof("CheckEtcdDataFileExists(): %s does not exist, waiting for kube", dataFilePath)
+			return false
+		}
+		// print error for debug but technically we can't say the file does not exist
+		klog.Errorf("CheckEtcdDataFileExists(): %v", err)
+	}
+	return true
 }
 
 //TODO: add to util
