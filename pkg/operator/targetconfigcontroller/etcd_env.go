4ba4bb198b955f831542108240811a9c750f2dd1
David Eads <deads@redhat.com>
2020-02-11 09:41:19 -0500

    fix env var host endpoints

--- pkg/operator/targetconfigcontroller/etcd_env.go
+++ pkg/operator/targetconfigcontroller/etcd_env.go
@@ -91,7 +91,7 @@ func getAllClusterMembers(envVarContext envVarContext) (map[string]string, error
 	}
 	for _, endpointAddress := range hostEtcdEndpoints.Subsets[0].Addresses {
 		if endpointAddress.Hostname == "etcd-bootstrap" {
-			endpoints = append(endpoints, fmt.Sprintf("etcd-bootstrap.%s", "alpha.installer.openshift.io/dns-suffix"))
+			endpoints = append(endpoints, fmt.Sprintf("etcd-bootstrap.%s", hostEtcdEndpoints.Annotations["alpha.installer.openshift.io/dns-suffix"]))
 			break
 		}
 	}
