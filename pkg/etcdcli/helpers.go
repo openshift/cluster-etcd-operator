5ec562ae8a2838652c1e05675fb00ef3c29c0ddb
Alay Patel <alay1431@gmail.com>
2020-03-11 16:04:54 -0400

    cmc: add unit tests to simulate failure

---
+++ pkg/etcdcli/helpers.go
@@ -0,0 +1,48 @@
+package etcdcli
+
+import (
+	"go.etcd.io/etcd/etcdserver/etcdserverpb"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+type fakeEtcdClient struct {
+	members []*etcdserverpb.Member
+}
+
+func (f *fakeEtcdClient) MemberAdd(peerURL string) error {
+	panic("implement me")
+}
+
+func (f *fakeEtcdClient) MemberList() ([]*etcdserverpb.Member, error) {
+	return f.members, nil
+}
+
+func (f *fakeEtcdClient) MemberRemove(member string) error {
+	panic("implement me")
+}
+
+func (f *fakeEtcdClient) UnhealthyMembers() ([]*etcdserverpb.Member, error) {
+	return []*etcdserverpb.Member{}, nil
+}
+
+func (f *fakeEtcdClient) MemberStatus(member *etcdserverpb.Member) string {
+	panic("implement me")
+}
+
+func (f *fakeEtcdClient) GetMember(name string) (*etcdserverpb.Member, error) {
+	for _, m := range f.members {
+		if m.Name == name {
+			return m, nil
+		}
+	}
+	return nil, apierrors.NewNotFound(schema.GroupResource{Group: "etcd.operator.openshift.io", Resource: "etcdmembers"}, name)
+}
+
+func (f *fakeEtcdClient) MemberUpdatePeerURL(id uint64, peerURL []string) error {
+	panic("implement me")
+}
+
+func NewFakeEtcdClient(members []*etcdserverpb.Member) EtcdClient {
+	return &fakeEtcdClient{members: members}
+}
