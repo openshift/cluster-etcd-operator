97a975eeeddd9750c41f32cd82073ed77793cf8d
Sam Batschelet <sbatsche@redhat.com>
2020-04-21 13:46:41 -0400

    pkg/cmd/render: add support for s390 and s390x arch
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

---
+++ pkg/cmd/render/env_test.go
@@ -0,0 +1,21 @@
+package render
+
+import "testing"
+
+func TestEtcdEnv(t *testing.T) {
+	for _, test := range []struct {
+		platform, arch, wantKey, wantValue string
+	}{
+		{"AWS", "amd64", "ETCD_HEARTBEAT_INTERVAL", "100"},
+		{"AWS", "amd64", "ETCD_ELECTION_TIMEOUT", "1000"},
+		{"Azure", "ppc64le", "ETCD_HEARTBEAT_INTERVAL", "500"},
+		{"Azure", "amd64", "ETCD_ELECTION_TIMEOUT", "2500"},
+		{"None", "ppc64le", "ETCD_HEARTBEAT_INTERVAL", "100"},
+		{"Azure", "s390x", "ETCD_UNSUPPORTED_ARCH", "s390x"},
+	} {
+		env, _ := getEtcdEnv(test.platform, test.arch)
+		if env[test.wantKey] != test.wantValue {
+			t.Errorf("getEtcdEnv(%q, %q) =  want %q = %q got %q = %q", test.platform, test.arch, test.wantKey, test.wantValue, test.wantKey, env[test.wantKey])
+		}
+	}
+}
