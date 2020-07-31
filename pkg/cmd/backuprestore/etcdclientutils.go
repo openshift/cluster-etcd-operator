872a7c3ff3dc60244d0378c813c24855bbc940b4
retroflexer <skolicha@redhat.com>
2020-07-31 10:01:03 -0400

    Converting cluster-backup and cluster-restore shell scripts as subcommands to cluster-etcd-operator.

---
+++ pkg/cmd/backuprestore/etcdclientutils.go
@@ -0,0 +1,74 @@
+package backuprestore
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"io"
+	"k8s.io/klog"
+	"os"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/pkg/transport"
+)
+
+func getEtcdClient(endpoints []string) (*clientv3.Client, error) {
+	dialOptions := []grpc.DialOption{
+		grpc.WithBlock(), // block until the underlying connection is up
+	}
+
+	tlsInfo := transport.TLSInfo{
+		CertFile:      os.Getenv("ETCDCTL_CERT"),
+		KeyFile:       os.Getenv("ETCDCTL_KEY"),
+		TrustedCAFile: os.Getenv("ETCDCTL_CACERT"),
+	}
+	tlsConfig, err := tlsInfo.ClientConfig()
+
+	cfg := &clientv3.Config{
+		DialOptions: dialOptions,
+		Endpoints:   endpoints,
+		DialTimeout: 2 * time.Second,
+		TLS:         tlsConfig,
+	}
+
+	cli, err := clientv3.New(*cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make etcd client for endpoints %v: %w", endpoints, err)
+	}
+	return cli, nil
+}
+
+func saveSnapshot(cli *clientv3.Client, dbPath string) error {
+	partpath := dbPath + ".part"
+	defer os.RemoveAll(partpath)
+
+	f, err := os.OpenFile(partpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("could not open %s (%w)", partpath, err)
+	}
+
+	opBegin := time.Now()
+	var rd io.ReadCloser
+	rd, err = cli.Snapshot(context.Background())
+	if err != nil {
+		return fmt.Errorf("saveSnapshot failed: %w", err)
+	}
+
+	if _, err := io.Copy(f, rd); err != nil {
+		return fmt.Errorf("saveSnapshot failed: %w", err)
+	}
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("saveSnapshot failed: %w", err)
+	}
+	klog.Info(
+		"fetched snapshot. ",
+		"Time taken: ", time.Since(opBegin),
+	)
+
+	if err := os.Rename(partpath, dbPath); err != nil {
+		return fmt.Errorf("could not rename %s to %s (%v)", partpath, dbPath, err)
+	}
+	klog.Info("saved snapshot to path", dbPath)
+	return nil
+}
---
+++ pkg/cmd/backuprestore/tarutils.go
@@ -0,0 +1,229 @@
+package backuprestore
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"io"
+	"k8s.io/klog"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func createTarball(tarballFilePath string, filePaths []string, prefixTrim string) error {
+	file, err := os.OpenFile(tarballFilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Could not create tarball file '%s', got error '%s'", tarballFilePath, err.Error()))
+	}
+	defer file.Close()
+
+	gzipWriter := gzip.NewWriter(file)
+	defer gzipWriter.Close()
+
+	tarWriter := tar.NewWriter(gzipWriter)
+	defer tarWriter.Close()
+
+	if prefixTrim != "" && !strings.HasSuffix(prefixTrim, "/") {
+		prefixTrim += "/"
+	}
+
+	for _, filePath := range filePaths {
+		err := addFileToTarWriter(filePath, tarWriter, prefixTrim)
+		if err != nil {
+			return errors.New(fmt.Sprintf("Could not add file '%s', to tarball, got error '%s'", filePath, err.Error()))
+		}
+	}
+
+	return nil
+}
+
+func addFileToTarWriter(src string, tarWriter *tar.Writer, prefixTrim string) error {
+
+	return filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
+
+		// return on any error
+		if err != nil {
+			return fmt.Errorf("addFileToTarWriter failed: %w", err)
+		}
+
+		// return on non-regular files
+		if !fi.Mode().IsRegular() {
+			return nil
+		}
+
+		header := &tar.Header{
+			Name:    file,
+			Size:    fi.Size(),
+			Mode:    int64(fi.Mode()),
+			ModTime: fi.ModTime(),
+		}
+
+		// update the name to correctly reflect the desired destination when untaring
+		header.Name = strings.TrimPrefix(file, prefixTrim)
+
+		// write the header
+		if err := tarWriter.WriteHeader(header); err != nil {
+			return fmt.Errorf("addFileToTarWriter: WriteHeader failed: %w", err)
+		}
+
+		// open files for taring
+		f, err := os.Open(file)
+		if err != nil {
+			return fmt.Errorf("addFileToTarWriter: file open failed: %w", err)
+		}
+
+		// copy file data into tar writer
+		if _, err := io.Copy(tarWriter, f); err != nil {
+			return fmt.Errorf("addFileToTarWriter: file write failed: %w", err)
+		}
+
+		// manually close here after each file operation; defering would cause each file close
+		// to wait until all operations have completed.
+		f.Close()
+
+		return nil
+	})
+
+}
+
+func extractFromTarGz(tarball, target string) (err error) {
+	r, err := os.Open(tarball)
+	if err != nil {
+		return err
+	}
+	t0 := time.Now()
+	nFiles := 0
+	madeDir := map[string]bool{}
+	defer func() {
+		td := time.Since(t0)
+		if err == nil {
+			klog.Infof("extracted tarball into %s: %d files, %d dirs (%v)", target, nFiles, len(madeDir), td)
+		} else {
+			klog.Infof("error extracting tarball into %s after %d files, %d dirs, %v: %v", target, nFiles, len(madeDir), td, err)
+		}
+	}()
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return fmt.Errorf("requires gzip-compressed body: %v", err)
+	}
+	tr := tar.NewReader(zr)
+	for {
+		f, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			klog.Errorf("tar reading error: %v", err)
+			return fmt.Errorf("tar error: %v", err)
+		}
+		if !validRelPath(f.Name) {
+			return fmt.Errorf("tar contained invalid name error %q", f.Name)
+		}
+		rel := filepath.FromSlash(f.Name)
+		abs := filepath.Join(target, rel)
+
+		fi := f.FileInfo()
+		mode := fi.Mode()
+		switch {
+		case mode.IsRegular():
+			// Make the directory. This is redundant because it should
+			// already be made by a directory entry in the tar
+			// beforehand. Thus, don't check for errors; the next
+			// write will fail with the same error.
+			dir := filepath.Dir(abs)
+			if !madeDir[dir] {
+				if err := os.MkdirAll(filepath.Dir(abs), 0755); err != nil {
+					return err
+				}
+				madeDir[dir] = true
+			}
+			wf, err := os.OpenFile(abs, os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode.Perm())
+			if err != nil {
+				return err
+			}
+			n, err := io.Copy(wf, tr)
+			if closeErr := wf.Close(); closeErr != nil && err == nil {
+				err = closeErr
+			}
+			if err != nil {
+				return fmt.Errorf("error writing to %s: %v", abs, err)
+			}
+			if n != f.Size {
+				return fmt.Errorf("only wrote %d bytes to %s; expected %d", n, abs, f.Size)
+			}
+			nFiles++
+		case mode.IsDir():
+			if err := os.MkdirAll(abs, 0755); err != nil {
+				return err
+			}
+			madeDir[abs] = true
+		default:
+			return fmt.Errorf("tar file entry %s contained unsupported file type %v", f.Name, mode)
+		}
+	}
+	return nil
+}
+
+func extractFileFromTarGz(tarball, targetdir, filebasename string) (err error) {
+	r, err := os.Open(tarball)
+	if err != nil {
+		return err
+	}
+	nFiles := 0
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return fmt.Errorf("requires gzip-compressed body: %v", err)
+	}
+	tr := tar.NewReader(zr)
+	for {
+		f, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			klog.Errorf("tar reading error: %v", err)
+			return fmt.Errorf("tar error: %v", err)
+		}
+		if !validRelPath(f.Name) {
+			return fmt.Errorf("tar contained invalid name error %q", f.Name)
+		}
+		if filepath.Base(f.Name) != filebasename {
+			continue
+		}
+		targetFile := filepath.Join(targetdir, filebasename)
+
+		fi := f.FileInfo()
+		mode := fi.Mode()
+		switch {
+		case mode.IsRegular():
+			wf, err := os.OpenFile(targetFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode.Perm())
+			if err != nil {
+				return err
+			}
+			n, err := io.Copy(wf, tr)
+			if closeErr := wf.Close(); closeErr != nil && err == nil {
+				err = closeErr
+			}
+			if err != nil {
+				return fmt.Errorf("error writing to %s: %v", targetFile, err)
+			}
+			if n != f.Size {
+				return fmt.Errorf("only wrote %d bytes to %s; expected %d", n, targetFile, f.Size)
+			}
+			nFiles++
+		default:
+			return fmt.Errorf("tar file entry %s contained unsupported file type %v", f.Name, mode)
+		}
+	}
+	return nil
+}
+
+func validRelPath(p string) bool {
+	if p == "" || strings.Contains(p, `\`) || strings.HasPrefix(p, "/") || strings.Contains(p, "../") {
+		return false
+	}
+	return true
+}
