484a12de71d3e9846bc5639830beaca021283c60
Sam Batschelet <sbatsche@redhat.com>
2020-08-11 17:33:11 -0400

    pkg/etcdenvvar: enable pprof by default
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- pkg/etcdenvvar/etcd_env.go
+++ pkg/etcdenvvar/etcd_env.go
@@ -29,6 +29,7 @@ var FixedEtcdEnvVars = map[string]string{
 	"ETCD_DATA_DIR":              "/var/lib/etcd",
 	"ETCD_QUOTA_BACKEND_BYTES":   "7516192768", // 7 gig
 	"ETCD_INITIAL_CLUSTER_STATE": "existing",
+	"ETCD_ENABLE_PPROF":          "true",
 }
 
 type envVarFunc func(envVarContext envVarContext) (map[string]string, error)
