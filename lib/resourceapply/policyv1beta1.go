45a2def09e4bf43693afd8b126659a5db02616da
Oleg Bulatov <oleg@bulatov.me>
2021-05-10 14:47:52 +0200

    Revert "Bug 1957498: Update policy.v1beta1 to v1 as it is deprecated in v1.21"

--- lib/resourceapply/policyv1beta1.go
+++ lib/resourceapply/policyv1beta1.go
@@ -3,15 +3,15 @@ package resourceapply
 import (
 	"context"
 	"github.com/openshift/cluster-etcd-operator/lib/resourcemerge"
-	"k8s.io/api/policy/v1"
+	"k8s.io/api/policy/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	policyv1 "k8s.io/client-go/kubernetes/typed/policy/v1"
+	policyv1beta1 "k8s.io/client-go/kubernetes/typed/policy/v1beta1"
 	"k8s.io/utils/pointer"
 )
 
 // ApplyPodDisruptionBudgets applies the required deployment to the cluster.
-func ApplyPodDisruptionBudgets(ctx context.Context, client policyv1.PodDisruptionBudgetsGetter, required *v1.PodDisruptionBudget) (*v1.PodDisruptionBudget, bool, error) {
+func ApplyPodDisruptionBudgets(ctx context.Context, client policyv1beta1.PodDisruptionBudgetsGetter, required *v1beta1.PodDisruptionBudget) (*v1beta1.PodDisruptionBudget, bool, error) {
 	existing, err := client.PodDisruptionBudgets(required.Namespace).Get(ctx, required.Name, metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
 		actual, err := client.PodDisruptionBudgets(required.Namespace).Create(ctx, required, metav1.CreateOptions{})
