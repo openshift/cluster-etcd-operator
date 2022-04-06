b13fbe65e057a93ae6c404e1cd13c499a03bb085
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2022-04-06 18:08:00 +0200

    adds attemptToRemoveLearningMember to the cluster member removal controller
    it attempts to remove a learning member pending deletion regardless of whether a replacement member has been found

--- pkg/operator/clustermemberremovalcontroller/clustermemberremovalcontroller_test.go
+++ pkg/operator/clustermemberremovalcontroller/clustermemberremovalcontroller_test.go
@@ -23,6 +23,145 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
+func TestAttemptToRemoveLearningMember(t *testing.T) {
+	scenarios := []struct {
+		name                                     string
+		initialObjectsForMachineLister           []runtime.Object
+		initialObjectsForConfigMapTargetNSLister []runtime.Object
+		initialEtcdMemberList                    []*etcdserverpb.Member
+		validateFn                               func(t *testing.T, fakeEtcdClient etcdcli.EtcdClient)
+	}{
+		{
+			name: "learning member pending deletion is removed",
+			initialObjectsForMachineLister: func() []runtime.Object {
+				m4 := machineWithHooksFor("m-4", "10.0.139.81")
+				m4.DeletionTimestamp = &metav1.Time{}
+				machines := wellKnownMasterMachines()
+				machines = append(machines, m4)
+				return machines
+			}(),
+			initialObjectsForConfigMapTargetNSLister: []runtime.Object{wellKnownEtcdEndpointsConfigMap()},
+			initialEtcdMemberList: func() []*etcdserverpb.Member {
+				members := append(wellKnownEtcdMemberList(), &etcdserverpb.Member{
+					Name:      "m-4",
+					ID:        4,
+					IsLearner: true,
+					PeerURLs:  []string{"https://10.0.139.81:1234"},
+				})
+				return members
+			}(),
+			validateFn: func(t *testing.T, fakeEtcdClient etcdcli.EtcdClient) {
+				memberList, err := fakeEtcdClient.MemberList(context.TODO())
+				if err != nil {
+					t.Fatal(err)
+				}
+				if len(memberList) != 3 {
+					t.Errorf("expected exactly 3 members, got %v", len(memberList))
+				}
+				for _, member := range memberList {
+					if member.ID == 4 {
+						t.Fatalf("expected the member: %v to be removed from the etcd cluster but it wasn't", member)
+					}
+				}
+			},
+		},
+
+		{
+			name:                  "voting member pending deletion is NOT removed",
+			initialEtcdMemberList: wellKnownEtcdMemberList(),
+			initialObjectsForMachineLister: func() []runtime.Object {
+				machines := wellKnownMasterMachines()
+				m0 := machines[0].(*machinev1beta1.Machine)
+				m0.DeletionTimestamp = &metav1.Time{}
+				return machines
+			}(),
+			initialObjectsForConfigMapTargetNSLister: []runtime.Object{wellKnownEtcdEndpointsConfigMap()},
+			validateFn: func(t *testing.T, fakeEtcdClient etcdcli.EtcdClient) {
+				memberList, err := fakeEtcdClient.MemberList(context.TODO())
+				if err != nil {
+					t.Fatal(err)
+				}
+				if len(memberList) != 3 {
+					t.Errorf("expected exactly 3 members, got %v", len(memberList))
+				}
+			},
+		},
+
+		{
+			name: "excessive voting member pending deletion is NOT removed",
+			initialEtcdMemberList: func() []*etcdserverpb.Member {
+				members := append(wellKnownEtcdMemberList(), &etcdserverpb.Member{
+					Name:     "m-4",
+					ID:       4,
+					PeerURLs: []string{"https://10.0.139.81:1234"},
+				})
+				return members
+			}(),
+			initialObjectsForMachineLister: func() []runtime.Object {
+				m4 := machineWithHooksFor("m-4", "10.0.139.81")
+				m4.DeletionTimestamp = &metav1.Time{}
+				machines := wellKnownMasterMachines()
+				machines = append(machines, m4)
+				return machines
+			}(),
+			initialObjectsForConfigMapTargetNSLister: func() []runtime.Object {
+				cm := wellKnownEtcdEndpointsConfigMap()
+				cm.Data["m-4"] = "10.0.139.81"
+				return []runtime.Object{cm}
+			}(),
+			validateFn: func(t *testing.T, fakeEtcdClient etcdcli.EtcdClient) {
+				memberList, err := fakeEtcdClient.MemberList(context.TODO())
+				if err != nil {
+					t.Fatal(err)
+				}
+				if len(memberList) != 4 {
+					t.Errorf("expected exactly 4 members, got %v", len(memberList))
+				}
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			// test data
+			configMapTargetNSIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
+			for _, initialObj := range scenario.initialObjectsForConfigMapTargetNSLister {
+				configMapTargetNSIndexer.Add(initialObj)
+			}
+			configMapTargetNSLister := corev1listers.NewConfigMapLister(configMapTargetNSIndexer).ConfigMaps("openshift-etcd")
+
+			machineIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
+			for _, initialObj := range scenario.initialObjectsForMachineLister {
+				machineIndexer.Add(initialObj)
+			}
+			machineLister := machinelistersv1beta1.NewMachineLister(machineIndexer)
+			machineSelector, err := labels.Parse("machine.openshift.io/cluster-api-machine-role=master")
+			if err != nil {
+				t.Fatal(err)
+			}
+			fakeEtcdClient, err := etcdcli.NewFakeEtcdClient(scenario.initialEtcdMemberList)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// act
+			target := clusterMemberRemovalController{
+				etcdClient:                        fakeEtcdClient,
+				masterMachineLister:               machineLister,
+				masterMachineSelector:             machineSelector,
+				configMapListerForTargetNamespace: configMapTargetNSLister,
+			}
+			err = target.attemptToRemoveLearningMember(context.TODO())
+			if err != nil {
+				t.Fatal(err)
+			}
+			if scenario.validateFn != nil {
+				scenario.validateFn(t, fakeEtcdClient)
+			}
+		})
+	}
+}
+
 func TestClusterMemberRemovalController(t *testing.T) {
 	alwaysTrueIsFunctionalMachineAPIFn := func() (bool, error) { return true, nil }
 
@@ -174,7 +313,9 @@ func wellKnownEtcdEndpointsConfigMap() *corev1.ConfigMap {
 	return &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{Name: "etcd-endpoints", Namespace: "openshift-etcd"},
 		Data: map[string]string{
-			"m-0": "10.0.139.78",
+			"m-1": "10.0.139.78",
+			"m-2": "10.0.139.79",
+			"m-3": "10.0.139.80",
 		},
 	}
 }
@@ -182,9 +323,19 @@ func wellKnownEtcdEndpointsConfigMap() *corev1.ConfigMap {
 func wellKnownEtcdMemberList() []*etcdserverpb.Member {
 	return []*etcdserverpb.Member{
 		{
-			Name:     "m-0",
-			ID:       8,
+			Name:     "m-1",
+			ID:       1,
 			PeerURLs: []string{"https://10.0.139.78:1234"},
+		},
+		{
+			Name:     "m-2",
+			ID:       2,
+			PeerURLs: []string{"https://10.0.139.79:1234"},
+		},
+		{
+			Name:     "m-3",
+			ID:       3,
+			PeerURLs: []string{"https://10.0.139.80:1234"},
 		},
 	}
 }
@@ -219,4 +370,30 @@ type fakeMachineAPI struct {
 
 func (dm *fakeMachineAPI) IsFunctional() (bool, error) {
 	return dm.isMachineAPIFunctional()
+}
+
+func machineFor(name, internalIP string) *machinev1beta1.Machine {
+	return &machinev1beta1.Machine{
+		ObjectMeta: metav1.ObjectMeta{Name: name, Labels: map[string]string{"machine.openshift.io/cluster-api-machine-role": "master"}},
+		Status: machinev1beta1.MachineStatus{Addresses: []corev1.NodeAddress{
+			{
+				Type:    corev1.NodeInternalIP,
+				Address: internalIP,
+			},
+		}},
+	}
+}
+
+func machineWithHooksFor(name, internalIP string) *machinev1beta1.Machine {
+	m := machineFor(name, internalIP)
+	m.Spec.LifecycleHooks.PreDrain = append(m.Spec.LifecycleHooks.PreDrain, machinev1beta1.LifecycleHook{Name: "EtcdQuorumOperator", Owner: "clusteroperator/etcd"})
+	return m
+}
+
+func wellKnownMasterMachines() []runtime.Object {
+	return []runtime.Object{
+		machineWithHooksFor("m-1", "10.0.139.78"),
+		machineWithHooksFor("m-2", "10.0.139.79"),
+		machineWithHooksFor("m-3", "10.0.139.80"),
+	}
 }
