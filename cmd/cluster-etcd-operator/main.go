ad69bd2de0bd571ae531f70e30a6573f39621fbb
Mustafa Elbehery <melbeher@redhat.com>
2022-06-30 14:19:09 +0200

    add etcd pod liveness and readiness probes

--- cmd/cluster-etcd-operator/main.go
+++ cmd/cluster-etcd-operator/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/backuprestore"
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/monitor"
 	operatorcmd "github.com/openshift/cluster-etcd-operator/pkg/cmd/operator"
+	"github.com/openshift/cluster-etcd-operator/pkg/cmd/readyz"
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/render"
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/waitforceo"
 	"github.com/openshift/cluster-etcd-operator/pkg/operator"
@@ -66,6 +67,7 @@ func NewSSCSCommand() *cobra.Command {
 	cmd.AddCommand(waitforceo.NewWaitForCeoCommand(os.Stderr))
 	cmd.AddCommand(monitor.NewMonitorCommand(os.Stderr))
 	cmd.AddCommand(verify.NewVerifyCommand(os.Stderr))
+	cmd.AddCommand(readyz.NewReadyzCommand())
 
 	return cmd
 }
