1af6ab3ef06ce77af40a4759da508b8d2af78594
Thomas Jungblut <tjungblu@redhat.com>
2022-07-04 15:24:13 +0200

    refactoring and unit test

---
+++ pkg/operator/targetconfigcontroller/targetconfigcontroller_test.go
@@ -0,0 +1,122 @@
+package targetconfigcontroller
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	configv1listers "github.com/openshift/client-go/config/listers/config/v1"
+	"github.com/openshift/library-go/pkg/controller/factory"
+	"github.com/openshift/library-go/pkg/operator/events"
+	"github.com/openshift/library-go/pkg/operator/v1helpers"
+	"github.com/stretchr/testify/assert"
+	"go.etcd.io/etcd/api/v3/etcdserverpb"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+
+	"github.com/openshift/cluster-etcd-operator/pkg/etcdcli"
+	"github.com/openshift/cluster-etcd-operator/pkg/etcdenvvar"
+	"github.com/openshift/cluster-etcd-operator/pkg/operator/operatorclient"
+	u "github.com/openshift/cluster-etcd-operator/pkg/testutils"
+)
+
+func TestTargetConfigController(t *testing.T) {
+
+	scenarios := []struct {
+		name            string
+		objects         []runtime.Object
+		staticPodStatus *operatorv1.StaticPodOperatorStatus
+		etcdMembers     []*etcdserverpb.Member
+		expectedErr     error
+	}{
+		{
+			name: "HappyPath",
+			objects: []runtime.Object{
+				u.BootstrapConfigMap(u.WithBootstrapStatus("complete")),
+			},
+			staticPodStatus: u.StaticPodOperatorStatus(
+				u.WithLatestRevision(3),
+				u.WithNodeStatusAtCurrentRevision(3),
+				u.WithNodeStatusAtCurrentRevision(3),
+				u.WithNodeStatusAtCurrentRevision(3),
+			),
+			etcdMembers: []*etcdserverpb.Member{
+				u.FakeEtcdMemberWithoutServer(0),
+				u.FakeEtcdMemberWithoutServer(1),
+				u.FakeEtcdMemberWithoutServer(2),
+			},
+		},
+		{
+			name: "Quorum not fault tolerant",
+			objects: []runtime.Object{
+				u.BootstrapConfigMap(u.WithBootstrapStatus("complete")),
+			},
+			staticPodStatus: u.StaticPodOperatorStatus(
+				u.WithLatestRevision(3),
+				u.WithNodeStatusAtCurrentRevision(3),
+				u.WithNodeStatusAtCurrentRevision(3),
+				u.WithNodeStatusAtCurrentRevision(3),
+			),
+			etcdMembers: []*etcdserverpb.Member{
+				u.FakeEtcdMemberWithoutServer(0),
+				u.FakeEtcdMemberWithoutServer(2),
+			},
+			expectedErr: fmt.Errorf("skipping TargetConfigController reconciliation due to insufficient quorum"),
+		},
+	}
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			fakeOperatorClient := v1helpers.NewFakeStaticPodOperatorClient(
+				&operatorv1.StaticPodOperatorSpec{
+					OperatorSpec: operatorv1.OperatorSpec{
+						ManagementState: operatorv1.Managed,
+					},
+				},
+				scenario.staticPodStatus,
+				nil,
+				nil,
+			)
+
+			fakeKubeClient := fake.NewSimpleClientset(scenario.objects...)
+			fakeEtcdClient, err := etcdcli.NewFakeEtcdClient(scenario.etcdMembers)
+			if err != nil {
+				t.Fatal(err)
+			}
+			eventRecorder := events.NewRecorder(fakeKubeClient.CoreV1().Events(operatorclient.TargetNamespace),
+				"test-targetconfigcontroller", &corev1.ObjectReference{})
+			indexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+			for _, obj := range scenario.objects {
+				if err := indexer.Add(obj); err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			envVar := etcdenvvar.FakeEnvVar{EnvVars: map[string]string{
+				"fake-key": "fake-value",
+			}}
+
+			controller := &TargetConfigController{
+				targetImagePullSpec:   "etcd-pull-spec",
+				operatorImagePullSpec: "operator-pull-spec",
+				operatorClient:        fakeOperatorClient,
+				kubeClient:            fakeKubeClient,
+				infrastructureLister:  configv1listers.NewInfrastructureLister(indexer),
+				networkLister:         configv1listers.NewNetworkLister(indexer),
+				configMapLister:       corev1listers.NewConfigMapLister(indexer),
+				endpointLister:        corev1listers.NewEndpointsLister(indexer),
+				nodeLister:            corev1listers.NewNodeLister(indexer),
+				envVarGetter:          envVar,
+				etcdClient:            fakeEtcdClient,
+				enqueueFn:             func() {},
+			}
+
+			err = controller.sync(context.TODO(), factory.NewSyncContext("test", eventRecorder))
+			assert.Equal(t, scenario.expectedErr, err)
+		})
+	}
+}
