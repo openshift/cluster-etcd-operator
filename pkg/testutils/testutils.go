8c02dd6aef12c85c1bc95655006b043111e3c6b9
Thomas Jungblut <tjungblu@redhat.com>
2022-08-26 11:34:28 +0200

    OCPBUGS-614: ensure healthy quorum before config update

--- pkg/testutils/testutils.go
+++ pkg/testutils/testutils.go
@@ -1,14 +1,16 @@
 package testutils
 
 import (
-	"encoding/base64"
 	"errors"
 	"fmt"
+	"math/rand"
+	"path/filepath"
+	"strings"
+	"testing"
+
 	configv1 "github.com/openshift/api/config/v1"
 	operatorv1 "github.com/openshift/api/operator/v1"
 	configv1listers "github.com/openshift/client-go/config/listers/config/v1"
-	"github.com/openshift/cluster-etcd-operator/pkg/etcdcli"
-	"github.com/openshift/cluster-etcd-operator/pkg/operator/operatorclient"
 	"go.etcd.io/etcd/api/v3/etcdserverpb"
 	"go.etcd.io/etcd/client/v3/mock/mockserver"
 	corev1 "k8s.io/api/core/v1"
@@ -20,8 +22,9 @@ import (
 	"k8s.io/apimachinery/pkg/util/uuid"
 	corev1listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
-	"path/filepath"
-	"testing"
+
+	"github.com/openshift/cluster-etcd-operator/pkg/dnshelpers"
+	"github.com/openshift/cluster-etcd-operator/pkg/operator/operatorclient"
 )
 
 func MustAbsPath(path string) string {
@@ -179,13 +182,21 @@ func WithBootstrapIP(ip string) func(*corev1.ConfigMap) {
 		if endpoints.Annotations == nil {
 			endpoints.Annotations = map[string]string{}
 		}
-		endpoints.Annotations[etcdcli.BootstrapIPAnnotationKey] = ip
+		// not relying on the constant from etcdcli.go here as this is introducing a cyclic dependency for its tests
+		endpoints.Annotations["alpha.installer.openshift.io/etcd-bootstrap"] = ip
 	}
 }
 
-func WithAddress(ip string) func(*corev1.ConfigMap) {
+func WithEndpoint(memberID uint64, peerURl string) func(*corev1.ConfigMap) {
+	if !strings.HasPrefix(peerURl, "https://") {
+		peerURl = "https://" + peerURl
+	}
+	ip, err := dnshelpers.GetIPFromAddress(peerURl)
+	if err != nil {
+		panic(err)
+	}
 	return func(endpoints *corev1.ConfigMap) {
-		endpoints.Data[base64.StdEncoding.WithPadding(base64.NoPadding).EncodeToString([]byte(ip))] = ip
+		endpoints.Data[fmt.Sprintf("%016x", memberID)] = ip
 	}
 }
 
@@ -207,7 +218,22 @@ func FakeEtcdMember(member int, etcdMock []*mockserver.MockServer) *etcdserverpb
 	return &etcdserverpb.Member{
 		Name:       fmt.Sprintf("etcd-%d", member),
 		ClientURLs: []string{etcdMock[member].Address},
+		PeerURLs:   []string{fmt.Sprintf("https://10.0.0.%d:2380", member+1)},
+		ID:         fakeMemberId(),
 	}
+}
+
+func FakeEtcdMemberWithoutServer(member int) *etcdserverpb.Member {
+	return &etcdserverpb.Member{
+		Name:       fmt.Sprintf("etcd-%d", member),
+		ClientURLs: []string{fmt.Sprintf("https://10.0.0.%d:2907", member+1)},
+		PeerURLs:   []string{fmt.Sprintf("https://10.0.0.%d:2380", member+1)},
+		ID:         uint64(member),
+	}
+}
+
+func fakeMemberId() uint64 {
+	return uint64(rand.Uint32())<<32 + uint64(rand.Uint32())
 }
 
 func FakeInfrastructureTopology(topologyMode configv1.TopologyMode) *configv1.Infrastructure {
@@ -223,6 +249,22 @@ func FakeConfigMap(namespace string, name string, data map[string]string) *corev
 	return &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{Namespace: namespace, Name: name},
 		Data:       data,
+	}
+}
+
+// FakeNetwork creates a fake network.
+// !isIpv6  serviceNetwork = []string{"10.0.1.0/24"}
+// isIPv6  serviceNetwork = []string{"2001:4860:4860::8888/32"}
+func FakeNetwork(isIPv6 bool) *configv1.Network {
+	var serviceNetwork []string
+	if isIPv6 {
+		serviceNetwork = []string{"2001:4860:4860::8888/32"}
+	} else {
+		serviceNetwork = []string{"10.0.1.0/24"}
+	}
+	return &configv1.Network{
+		ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
+		Status:     configv1.NetworkStatus{ServiceNetwork: serviceNetwork},
 	}
 }
 
@@ -303,17 +345,7 @@ func FakeClusterVersionLister(t *testing.T, clusterVersion *configv1.ClusterVers
 // isIPv6  serviceNetwork = []string{"2001:4860:4860::8888/32"}
 func FakeNetworkLister(t *testing.T, isIPv6 bool) configv1listers.NetworkLister {
 	indexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
-	var serviceNetwork []string
-	if isIPv6 {
-		serviceNetwork = []string{"2001:4860:4860::8888/32"}
-	} else {
-		serviceNetwork = []string{"10.0.1.0/24"}
-	}
-	fakeNetworkConfig := &configv1.Network{
-		ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-		Status:     configv1.NetworkStatus{ServiceNetwork: serviceNetwork},
-	}
-	if err := indexer.Add(fakeNetworkConfig); err != nil {
+	if err := indexer.Add(FakeNetwork(isIPv6)); err != nil {
 		t.Fatal(err.Error())
 	}
 	return configv1listers.NewNetworkLister(indexer)
