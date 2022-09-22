7b8cfd6bab0f69765ef7ae5e87871eef37d5a1c8
Stanislav Laznicka <slaznick@redhat.com>
2022-09-22 16:24:00 +0200

    tlshelpers: remove comment on SKID and AKID
    SKID and AKID are being supplied automatically by library-go helpers.
    If they weren't, golang supplies them automatically wherever necessary.

--- pkg/tlshelpers/tlshelpers.go
+++ pkg/tlshelpers/tlshelpers.go
@@ -82,12 +82,6 @@ func createNewCombinedClientAndServingCerts(caCert, caKey []byte, podFQDN, org s
 		// TODO: Extended Key Usage:
 		// All profiles expect a x509.ExtKeyUsageCodeSigning set on extended Key Usages
 		// need to investigage: https://github.com/etcd-io/etcd/issues/9398#issuecomment-435340312
-		// TODO: some extensions are missing form cfssl.
-		// e.g.
-		//	X509v3 Subject Key Identifier:
-		//		B7:30:0B:CF:47:4E:21:AE:13:60:74:42:B0:D9:C4:F3:26:69:63:03
-		//	X509v3 Authority Key Identifier:
-		//		keyid:9B:C0:6B:0C:8E:5C:73:6A:83:B1:E4:54:97:D3:62:18:8A:9C:BC:1E
 		// TODO: Change serial number logic, to something as follows.
 		// The following is taken from CFSSL library.
 		// If CFSSL is providing the serial numbers, it makes
