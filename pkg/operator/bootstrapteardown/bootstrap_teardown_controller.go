3d7fff63e9dd8db43e1a2ff1ec8b0930402b48ad
Thomas Jungblut <tjungblu@redhat.com>
2023-01-26 18:14:06 +0100

    OCPBUGS-5988: add dedicated success status for bootstrap removal (#992)
    * OCPBUGS-5988: add dedicated success status for bootstrap removal
    
    * wrap errors for increased visibility
    
    * fix wrong error condition
    
    * refactor and add tests

--- pkg/operator/bootstrapteardown/bootstrap_teardown_controller.go
+++ pkg/operator/bootstrapteardown/bootstrap_teardown_controller.go
@@ -12,6 +12,7 @@ import (
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	corev1listers "k8s.io/client-go/listers/core/v1"
+	"k8s.io/klog/v2"
 
 	"github.com/openshift/cluster-etcd-operator/pkg/etcdcli"
 	"github.com/openshift/cluster-etcd-operator/pkg/operator/ceohelpers"
@@ -46,8 +47,21 @@ func NewBootstrapTeardownController(
 	).WithSync(c.sync).ToController("BootstrapTeardownController", eventRecorder.WithComponentSuffix("bootstrap-teardown-controller"))
 }
 
-func (c *BootstrapTeardownController) sync(ctx context.Context, syncCtx factory.SyncContext) error {
-	err := c.removeBootstrap(ctx, syncCtx)
+func (c *BootstrapTeardownController) sync(ctx context.Context, _ factory.SyncContext) error {
+	timeoutCtx, cancelFunc := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancelFunc()
+
+	scalingStrategy, err := ceohelpers.GetBootstrapScalingStrategy(c.operatorClient, c.namespaceLister, c.infrastructureLister)
+	if err != nil {
+		return fmt.Errorf("failed to get bootstrap scaling strategy: %w", err)
+	}
+	// checks the actual etcd cluster membership API if etcd-bootstrap exists
+	safeToRemoveBootstrap, hasBootstrap, bootstrapID, err := c.canRemoveEtcdBootstrap(ctx, scalingStrategy)
+	if err != nil {
+		return fmt.Errorf("error while canRemoveEtcdBootstrap: %w", err)
+	}
+
+	err = c.removeBootstrap(timeoutCtx, safeToRemoveBootstrap, hasBootstrap, bootstrapID)
 	if err != nil {
 		_, _, updateErr := v1helpers.UpdateStatus(ctx, c.operatorClient, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
 			Type:    "BootstrapTeardownDegraded",
@@ -56,7 +70,7 @@ func (c *BootstrapTeardownController) sync(ctx context.Context, syncCtx factory.
 			Message: err.Error(),
 		}))
 		if updateErr != nil {
-			syncCtx.Recorder().Warning("BootstrapTeardownErrorUpdatingStatus", updateErr.Error())
+			klog.Errorf("error while updating BootstrapTeardownDegraded status with actual error [%v], [%v]", err, updateErr)
 		}
 		return err
 	}
@@ -70,31 +84,31 @@ func (c *BootstrapTeardownController) sync(ctx context.Context, syncCtx factory.
 	return updateErr
 }
 
-func (c *BootstrapTeardownController) removeBootstrap(ctx context.Context, syncCtx factory.SyncContext) error {
-	scalingStrategy, err := ceohelpers.GetBootstrapScalingStrategy(c.operatorClient, c.namespaceLister, c.infrastructureLister)
-	if err != nil {
-		return fmt.Errorf("failed to get bootstrap scaling strategy: %w", err)
-	}
-	// checks the actual etcd cluster membership API if etcd-bootstrap exists
-	safeToRemoveBootstrap, hasBootstrap, bootstrapID, err := c.canRemoveEtcdBootstrap(ctx, scalingStrategy)
-	switch {
-	case err != nil:
-		return err
+func (c *BootstrapTeardownController) removeBootstrap(ctx context.Context, safeToRemoveBootstrap bool, hasBootstrap bool, bootstrapID uint64) error {
+	if !hasBootstrap {
+		klog.V(4).Infof("no bootstrap anymore setting removal status")
+		// this is to ensure the status is always set correctly, even if the status update below failed
+		updateErr := setSuccessfulBoostrapRemovalStatus(ctx, c.operatorClient)
+		if updateErr != nil {
+			return fmt.Errorf("error while setSuccessfulBoostrapRemovalStatus: %w", updateErr)
+		}
 
-	case !hasBootstrap:
 		// if the bootstrap isn't present, then clearly we're available enough to terminate. This avoids any risk of flapping.
-		_, _, updateErr := v1helpers.UpdateStatus(ctx, c.operatorClient, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
+		_, _, updateErr = v1helpers.UpdateStatus(ctx, c.operatorClient, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
 			Type:    "EtcdRunningInCluster",
 			Status:  operatorv1.ConditionTrue,
 			Reason:  "BootstrapAlreadyRemoved",
 			Message: "etcd-bootstrap member is already removed",
 		}))
-		if updateErr != nil {
-			return updateErr
-		}
-		return nil
 
-	case !safeToRemoveBootstrap:
+		if updateErr != nil {
+			return fmt.Errorf("error while updating BootstrapAlreadyRemoved: %w", updateErr)
+		}
+
+		return nil
+	}
+
+	if !safeToRemoveBootstrap {
 		_, _, updateErr := v1helpers.UpdateStatus(ctx, c.operatorClient, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
 			Type:    "EtcdRunningInCluster",
 			Status:  operatorv1.ConditionFalse,
@@ -102,10 +116,9 @@ func (c *BootstrapTeardownController) removeBootstrap(ctx context.Context, syncC
 			Message: "still waiting for three healthy etcd members",
 		}))
 		if updateErr != nil {
-			return updateErr
+			return fmt.Errorf("error while updating NotEnoughEtcdMembers: %w", updateErr)
 		}
 		return nil
-
 	}
 
 	_, _, updateErr := v1helpers.UpdateStatus(ctx, c.operatorClient, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
@@ -115,31 +128,47 @@ func (c *BootstrapTeardownController) removeBootstrap(ctx context.Context, syncC
 		Message: "enough members found",
 	}))
 	if updateErr != nil {
-		return updateErr
+		return fmt.Errorf("error while updating EnoughEtcdMembers: %w", updateErr)
 	}
 
 	// check to see if bootstrapping is complete
 	cmNamespace := "kube-system"
 	cmName := "bootstrap"
 	bootstrapFinishedConfigMap, err := c.configmapLister.ConfigMaps(cmNamespace).Get(cmName)
-	switch {
-	case apierrors.IsNotFound(err):
-		syncCtx.Recorder().Event("DelayingBootstrapTeardown", fmt.Sprintf("cluster-bootstrap is not yet finished - ConfigMap '%s/%s' not found", cmNamespace, cmName))
-		return nil
-	case err != nil:
-		return err
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			klog.Warningf("cluster-bootstrap is not yet finished - ConfigMap '%s/%s' not found", cmNamespace, cmName)
+			return nil
+		}
+		return fmt.Errorf("error while getting bootstrap configmap: %w", err)
+	}
 
-	case bootstrapFinishedConfigMap.Data["status"] != "complete":
-		syncCtx.Recorder().Event("DelayingBootstrapTeardown", "cluster-bootstrap is not yet finished")
+	if bootstrapFinishedConfigMap.Data["status"] != "complete" {
+		klog.Warningf("cluster-bootstrap is not yet finished, bootstrap status=[%s]", bootstrapFinishedConfigMap.Data["status"])
 		return nil
 	}
 
-	syncCtx.Recorder().Event("RemoveBootstrapEtcd", "removing etcd-bootstrap member")
+	klog.Warningf("Removing bootstrap member [%x]", bootstrapID)
+
 	// this is ugly until bootkube is updated, but we want to be sure that bootkube has time to be waiting to watch the condition coming back.
 	if err := c.etcdClient.MemberRemove(ctx, bootstrapID); err != nil {
-		return err
+		return fmt.Errorf("error while removing bootstrap member [%x]: %w", bootstrapID, err)
 	}
-	return nil
+
+	klog.Infof("Successfully removed bootstrap member [%x]", bootstrapID)
+	// below might fail, since the member removal can cause some downtime for raft to settle on a quorum
+	// it's important that everything below is properly retried above during normal controller reconciliation
+	return setSuccessfulBoostrapRemovalStatus(ctx, c.operatorClient)
+}
+
+func setSuccessfulBoostrapRemovalStatus(ctx context.Context, client v1helpers.StaticPodOperatorClient) error {
+	_, _, updateErr := v1helpers.UpdateStatus(ctx, client, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
+		Type:    "EtcdBoostrapMemberRemoved",
+		Status:  operatorv1.ConditionTrue,
+		Reason:  "BootstrapMemberRemoved",
+		Message: "etcd bootstrap member is removed",
+	}))
+	return updateErr
 }
 
 // canRemoveEtcdBootstrap returns whether it is safe to remove bootstrap, whether bootstrap is in the list, and an error
--- pkg/operator/bootstrapteardown/bootstrap_teardown_controller_test.go
+++ pkg/operator/bootstrapteardown/bootstrap_teardown_controller_test.go
@@ -2,13 +2,68 @@ package bootstrapteardown
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	operatorv1 "github.com/openshift/api/operator/v1"
+	configv1listers "github.com/openshift/client-go/config/listers/config/v1"
 	"github.com/openshift/cluster-etcd-operator/pkg/etcdcli"
 	"github.com/openshift/cluster-etcd-operator/pkg/operator/ceohelpers"
 	u "github.com/openshift/cluster-etcd-operator/pkg/testutils"
+	"github.com/openshift/library-go/pkg/operator/v1helpers"
 	"github.com/stretchr/testify/require"
 	"go.etcd.io/etcd/api/v3/etcdserverpb"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	bootstrapComplete = &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "bootstrap", Namespace: "kube-system"},
+		Data:       map[string]string{"status": "complete"},
+	}
+
+	conditionBootstrapAlreadyRemoved = operatorv1.OperatorCondition{
+		Type:    "EtcdRunningInCluster",
+		Status:  "True",
+		Reason:  "BootstrapAlreadyRemoved",
+		Message: "etcd-bootstrap member is already removed",
+	}
+
+	conditionBootstrapMemberRemoved = operatorv1.OperatorCondition{
+		Type:    "EtcdBoostrapMemberRemoved",
+		Status:  "True",
+		Reason:  "BootstrapMemberRemoved",
+		Message: "etcd bootstrap member is removed",
+	}
+
+	conditionWaitingForEtcdMembers = operatorv1.OperatorCondition{
+		Type:    "EtcdRunningInCluster",
+		Status:  "False",
+		Reason:  "NotEnoughEtcdMembers",
+		Message: "still waiting for three healthy etcd members",
+	}
+
+	conditionEnoughEtcdMembers = operatorv1.OperatorCondition{
+		Type:    "EtcdRunningInCluster",
+		Status:  "True",
+		Reason:  "EnoughEtcdMembers",
+		Message: "enough members found",
+	}
+
+	conditionEtcdMemberRemoved = operatorv1.OperatorCondition{
+		Type:    "EtcdBoostrapMemberRemoved",
+		Status:  "True",
+		Reason:  "BootstrapMemberRemoved",
+		Message: "etcd bootstrap member is removed",
+	}
+
+	bootstrapProgressing = &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "bootstrap", Namespace: "kube-system"},
+		Data:       map[string]string{"status": "progressing"},
+	}
 )
 
 func TestCanRemoveEtcdBootstrap(t *testing.T) {
@@ -139,5 +194,133 @@ func TestCanRemoveEtcdBootstrap(t *testing.T) {
 			require.Equal(t, test.bootstrapId, bootstrapId, "bootstrap id")
 		})
 	}
+}
 
+func TestRemoveBootstrap(t *testing.T) {
+
+	tests := map[string]struct {
+		safeToRemove       bool
+		hasBootstrap       bool
+		bootstrapId        uint64
+		expectedConditions []operatorv1.OperatorCondition
+		indexerObjs        []interface{}
+		expectedErr        error
+	}{
+		"unsafe, no bootstrap": {
+			safeToRemove: false,
+			hasBootstrap: false,
+			bootstrapId:  0,
+			expectedConditions: []operatorv1.OperatorCondition{
+				conditionBootstrapMemberRemoved,
+				conditionBootstrapAlreadyRemoved,
+			},
+		},
+		"safe, no bootstrap": {
+			safeToRemove: true,
+			hasBootstrap: false,
+			bootstrapId:  0,
+			expectedConditions: []operatorv1.OperatorCondition{
+				conditionBootstrapMemberRemoved,
+				conditionBootstrapAlreadyRemoved,
+			},
+		},
+		"unsafe, has bootstrap": {
+			safeToRemove: false,
+			hasBootstrap: true,
+			bootstrapId:  0,
+			expectedConditions: []operatorv1.OperatorCondition{
+				conditionWaitingForEtcdMembers,
+			},
+		},
+		"safe, has bootstrap, incomplete process": {
+			safeToRemove: true,
+			hasBootstrap: true,
+			bootstrapId:  0,
+			expectedConditions: []operatorv1.OperatorCondition{
+				conditionEnoughEtcdMembers,
+			},
+		},
+		"safe, has bootstrap, incomplete process progressing": {
+			safeToRemove: true,
+			hasBootstrap: true,
+			bootstrapId:  0,
+			expectedConditions: []operatorv1.OperatorCondition{
+				conditionEnoughEtcdMembers,
+			},
+			indexerObjs: []interface{}{
+				bootstrapProgressing,
+			},
+		},
+		"safe, has bootstrap, complete process, fails removal": {
+			safeToRemove: true,
+			hasBootstrap: true,
+			bootstrapId:  0,
+			expectedConditions: []operatorv1.OperatorCondition{
+				conditionEnoughEtcdMembers,
+			},
+			indexerObjs: []interface{}{
+				bootstrapComplete,
+			},
+			expectedErr: fmt.Errorf("error while removing bootstrap member [%x]: %w", 0, fmt.Errorf("member with the given ID: 0 doesn't exist")),
+		},
+		"safe, has bootstrap, complete process, successful removal": {
+			safeToRemove: true,
+			hasBootstrap: true,
+			bootstrapId:  1,
+			expectedConditions: []operatorv1.OperatorCondition{
+				conditionEnoughEtcdMembers,
+				conditionEtcdMemberRemoved,
+			},
+			indexerObjs: []interface{}{
+				bootstrapComplete,
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+
+			indexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+			for _, obj := range test.indexerObjs {
+				require.NoError(t, indexer.Add(obj))
+			}
+
+			fakeNamespaceLister := corev1listers.NewNamespaceLister(indexer)
+			fakeConfigmapLister := corev1listers.NewConfigMapLister(indexer)
+			fakeInfraLister := configv1listers.NewInfrastructureLister(indexer)
+			fakeStaticPodClient := v1helpers.NewFakeStaticPodOperatorClient(&operatorv1.StaticPodOperatorSpec{}, &operatorv1.StaticPodOperatorStatus{}, nil, nil)
+			fakeEtcdClient, err := etcdcli.NewFakeEtcdClient([]*etcdserverpb.Member{u.FakeEtcdBoostrapMember(1)})
+			require.NoError(t, err)
+
+			c := &BootstrapTeardownController{
+				fakeStaticPodClient,
+				fakeEtcdClient,
+				fakeConfigmapLister,
+				fakeNamespaceLister,
+				fakeInfraLister,
+			}
+
+			err = c.removeBootstrap(context.TODO(), test.safeToRemove, test.hasBootstrap, test.bootstrapId)
+			require.Equal(t, test.expectedErr, err)
+
+			_, status, _, err := fakeStaticPodClient.GetStaticPodOperatorState()
+			require.NoError(t, err)
+			require.ElementsMatch(t, test.expectedConditions, removeTransitionTime(status.Conditions))
+		})
+	}
+}
+
+// removeTransitionTime will create a new list of operator conditions without the LastTransitionTime.
+// We need to remove the time component to be able to match the structs in require.ElementsMatch
+func removeTransitionTime(conditions []operatorv1.OperatorCondition) []operatorv1.OperatorCondition {
+	var timelessConditions []operatorv1.OperatorCondition
+	for _, c := range conditions {
+		timelessConditions = append(timelessConditions, operatorv1.OperatorCondition{
+			Type:    c.Type,
+			Status:  c.Status,
+			Reason:  c.Reason,
+			Message: c.Message,
+		})
+	}
+	return timelessConditions
 }
