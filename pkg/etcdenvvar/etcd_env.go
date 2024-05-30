14b4cf1e393fed578658e933b011c643a2424359
Devan Goodwin <dgoodwin@redhat.com>
2024-05-30 08:24:37 -0300

    Revert "ETCD-606: Batch bundle revision rollout (#1268)"
    This reverts commit 45972c187ec4f96f99db181f99ab62e3efb3ca79.

--- pkg/etcdenvvar/etcd_env.go
+++ pkg/etcdenvvar/etcd_env.go
@@ -126,7 +126,7 @@ func getEtcdctlEnvVars(envVarContext envVarContext) (map[string]string, error) {
 	}
 	return map[string]string{
 		"ETCDCTL_API":       "3",
-		"ETCDCTL_CACERT":    "/etc/kubernetes/static-pod-certs/configmaps/etcd-all-bundles/server-ca-bundle.crt",
+		"ETCDCTL_CACERT":    "/etc/kubernetes/static-pod-certs/configmaps/etcd-serving-ca/ca-bundle.crt",
 		"ETCDCTL_CERT":      "/etc/kubernetes/static-pod-certs/secrets/etcd-all-certs/etcd-peer-NODE_NAME.crt",
 		"ETCDCTL_KEY":       "/etc/kubernetes/static-pod-certs/secrets/etcd-all-certs/etcd-peer-NODE_NAME.key",
 		"ETCDCTL_ENDPOINTS": endpoints,
