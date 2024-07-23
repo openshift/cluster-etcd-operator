fec0685619ff5bc73e7593108c8588796ba44fa2
Mustafa Elbehery <melbeher@redhat.com>
2024-07-23 11:26:08 +0200

    ETCD-635: add backup server sidecar

---
+++ pkg/backuphelpers/backupvars_test.go
@@ -0,0 +1,96 @@
+package backuphelpers
+
+import (
+	"testing"
+
+	backupv1alpha1 "github.com/openshift/api/config/v1alpha1"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	schedule = "0 */2 * * *"
+	timezone = "GMT"
+)
+
+func TestBackupConfig_ToArgs(t *testing.T) {
+	testCases := []struct {
+		name     string
+		cr       backupv1alpha1.EtcdBackupSpec
+		enabled  bool
+		expected string
+	}{
+		{
+			"enabled",
+			createEtcdBackupSpec(timezone, schedule),
+			true,
+			"    - backup-server\n    - --enabled=true\n    - --timezone=GMT\n    - --schedule=0 */2 * * *",
+		},
+		{
+			"disabled",
+			createEtcdBackupSpec(timezone, schedule),
+			false,
+			"    - backup-server\n    - --enabled=false\n    - --timezone=GMT\n    - --schedule=0 */2 * * *",
+		},
+		{
+			"empty schedule disabled",
+			createEtcdBackupSpec(timezone, ""),
+			false,
+			"    - backup-server\n    - --enabled=false\n    - --timezone=GMT",
+		},
+		{
+			"empty schedule enabled",
+			createEtcdBackupSpec(timezone, ""),
+			true,
+			"    - backup-server\n    - --enabled=true\n    - --timezone=GMT",
+		},
+
+		{
+			"empty timezone disabled",
+			createEtcdBackupSpec("", schedule),
+			false,
+			"    - backup-server\n    - --enabled=false\n    - --schedule=0 */2 * * *",
+		},
+		{
+			"empty timezone enabled",
+			createEtcdBackupSpec("", schedule),
+			true,
+			"    - backup-server\n    - --enabled=true\n    - --schedule=0 */2 * * *",
+		},
+
+		{
+			"empty timezone and schedule disabled",
+			createEtcdBackupSpec("", ""),
+			false,
+			"    - backup-server\n    - --enabled=false",
+		},
+		{
+			"empty timezone and schedule enabled",
+			createEtcdBackupSpec("", ""),
+			true,
+			"    - backup-server\n    - --enabled=true",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			c := NewDisabledBackupConfig()
+			c.SetBackupSpec(tc.cr)
+			if tc.enabled {
+				c.enabled = true
+			}
+
+			act := c.ArgString()
+
+			require.Equal(t, tc.expected, act)
+		})
+	}
+}
+
+func createEtcdBackupSpec(timezone, schedule string) backupv1alpha1.EtcdBackupSpec {
+	return backupv1alpha1.EtcdBackupSpec{
+		Schedule: schedule,
+		TimeZone: timezone,
+	}
+}
---
+++ pkg/cmd/backuprestore/backupserver.go
@@ -0,0 +1,105 @@
+package backuprestore
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"k8s.io/klog/v2"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+var shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
+
+type backupServer struct {
+	schedule string
+	timeZone string
+	enabled  bool
+	//scheduler *tasker.Tasker
+	backupOptions
+}
+
+func NewBackupServer(ctx context.Context) *cobra.Command {
+	backupSrv := &backupServer{
+		backupOptions: backupOptions{errOut: os.Stderr},
+	}
+
+	cmd := &cobra.Command{
+		Use:   "backup-server",
+		Short: "Backs up a snapshot of etcd database and static pod resources without config",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			klog.Infof("hello from backup server :) ")
+
+			if err := backupSrv.Validate(); err != nil {
+				klog.Fatal(err)
+			}
+			if err := backupSrv.Run(ctx); err != nil {
+				klog.Fatal(err)
+			}
+		},
+	}
+	backupSrv.AddFlags(cmd.Flags())
+	return cmd
+}
+
+func (b *backupServer) AddFlags(fs *pflag.FlagSet) {
+	fs.BoolVar(&b.enabled, "enabled", false, "enable backup server")
+	fs.StringVar(&b.schedule, "schedule", "", "schedule specifies the cron schedule to run the backup")
+	fs.StringVar(&b.timeZone, "timezone", "", "timezone specifies the timezone of the cron schedule to run the backup")
+	cobra.MarkFlagRequired(fs, "enabled")
+
+	//b.backupOptions.AddFlags(fs)
+}
+
+func (b *backupServer) Validate() error {
+	//return b.backupOptions.Validate()
+	return nil
+}
+
+func (b *backupServer) Run(ctx context.Context) error {
+	//b.scheduler = tasker.New(tasker.Option{
+	//	Verbose: true,
+	//	Tz:      b.timeZone,
+	//})
+
+	klog.Infof("hello from backup server Run() :) ")
+	// handle teardown
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	shutdownHandler := make(chan os.Signal, 2)
+	signal.Notify(shutdownHandler, shutdownSignals...)
+	go func() {
+		select {
+		case <-shutdownHandler:
+			klog.Infof("Received SIGTERM or SIGINT signal, shutting down.")
+			close(shutdownHandler)
+			cancel()
+		case <-ctx.Done():
+			klog.Infof("Context has been cancelled, shutting down.")
+			close(shutdownHandler)
+			cancel()
+		}
+	}()
+
+	//err := b.scheduleBackup()
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//doneCh := make(chan struct{})
+	//go func() {
+	//	b.scheduler.Run()
+	//	doneCh <- struct{}{}
+	//}()
+	//
+	//<-doneCh
+	//return nil
+
+	<-ctx.Done()
+
+	return nil
+}
