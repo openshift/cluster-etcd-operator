95f6f5c5e215c4e821449b8cbcad3a92c4995485
Thomas Jungblut <tjungblu@redhat.com>
2024-08-16 09:54:26 +0200

    ETCD-654: add quorum-restore script
    this adds the initial take of a quorum restore script.
    
    Signed-off-by: Thomas Jungblut <tjungblu@redhat.com>

--- pkg/operator/scriptcontroller/scriptcontroller.go
+++ pkg/operator/scriptcontroller/scriptcontroller.go
@@ -123,6 +123,7 @@ func (c *ScriptController) manageScriptConfigMap(ctx context.Context, recorder e
 
 	for _, filename := range []string{
 		"etcd/cluster-restore.sh",
+		"etcd/quorum-restore.sh",
 		"etcd/cluster-backup.sh",
 		"etcd/etcd-common-tools",
 	} {
