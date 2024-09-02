aeacf5c7fc8a24e09feffd3774c72c4a9fe27de5
Mustafa Elbehery <melbeher@redhat.com>
2024-09-02 08:45:02 +0200

    ETCD-659: Add `etcd-backup-server` sidecar skeleton (#1304)
    * ETCD-659: Add etcd-backup-server sidecar skeleton
    
    * remove COMPUTED_BACKUP_VARS
    
    * fix manifest
    
    * using NotifyContext()
    
    * add backup-server to CEO

---
+++ pkg/cmd/backuprestore/backupserver.go
@@ -0,0 +1,65 @@
+package backuprestore
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"k8s.io/klog/v2"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+var shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
+
+type backupServer struct {
+	schedule string
+	timeZone string
+	enabled  bool
+	backupOptions
+}
+
+func NewBackupServer(ctx context.Context) *cobra.Command {
+	backupSrv := &backupServer{
+		backupOptions: backupOptions{errOut: os.Stderr},
+	}
+
+	cmd := &cobra.Command{
+		Use:   "backup-server",
+		Short: "Backs up a snapshot of etcd database and static pod resources without config",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			if err := backupSrv.Validate(); err != nil {
+				klog.Fatal(err)
+			}
+			if err := backupSrv.Run(ctx); err != nil {
+				klog.Fatal(err)
+			}
+		},
+	}
+	backupSrv.AddFlags(cmd.Flags())
+	return cmd
+}
+
+func (b *backupServer) AddFlags(fs *pflag.FlagSet) {
+	fs.BoolVar(&b.enabled, "enabled", false, "enable backup server")
+	fs.StringVar(&b.schedule, "schedule", "", "schedule specifies the cron schedule to run the backup")
+	fs.StringVar(&b.timeZone, "timezone", "", "timezone specifies the timezone of the cron schedule to run the backup")
+}
+
+func (b *backupServer) Validate() error {
+	return nil
+}
+
+func (b *backupServer) Run(ctx context.Context) error {
+	// handle teardown
+	cCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	signal.NotifyContext(cCtx, shutdownSignals...)
+
+	<-ctx.Done()
+
+	return nil
+}
