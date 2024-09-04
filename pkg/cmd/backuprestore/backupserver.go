f47d4b7cb50561c50c0c28baadbd58c8ad753487
Mustafa Elbehery <melbeher@redhat.com>
2024-09-04 14:33:09 +0200

    validating schedule

--- pkg/cmd/backuprestore/backupserver.go
+++ pkg/cmd/backuprestore/backupserver.go
@@ -2,7 +2,6 @@ package backuprestore
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"os"
 	"os/signal"
@@ -21,9 +20,10 @@ const backupVolume = "/var/backup/etcd/"
 var shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
 
 type backupServer struct {
-	schedule string
-	timeZone string
-	enabled  bool
+	schedule     string
+	timeZone     string
+	enabled      bool
+	cronSchedule cron.Schedule
 	backupOptions
 }
 
@@ -55,6 +55,8 @@ func (b *backupServer) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&b.schedule, "schedule", "", "schedule specifies the cron schedule to run the backup")
 	fs.StringVar(&b.timeZone, "timezone", "", "timezone specifies the timezone of the cron schedule to run the backup")
 
+	cobra.MarkFlagRequired(fs, "schedule")
+
 	b.backupOptions.AddFlags(fs)
 }
 
@@ -64,14 +66,16 @@ func (b *backupServer) Validate() error {
 		return nil
 	}
 
-	if len(b.schedule) == 0 {
-		err := errors.New("missing required flag: --schedule")
-		klog.Error(err)
-		return err
+	cronSchedule, err := cron.ParseStandard(b.schedule)
+	if err != nil {
+		pErr := fmt.Errorf("error parsing backup schedule %v: %w", b.schedule, err)
+		klog.Error(pErr)
+		return pErr
 	}
+	b.cronSchedule = cronSchedule
 
 	b.backupOptions.backupDir = backupVolume
-	err := b.backupOptions.Validate()
+	err = b.backupOptions.Validate()
 	if err != nil {
 		klog.Error(err)
 		return err
@@ -87,14 +91,7 @@ func (b *backupServer) Run(ctx context.Context) error {
 	signal.NotifyContext(cCtx, shutdownSignals...)
 
 	if b.enabled {
-		schedule, err := cron.ParseStandard(b.schedule)
-		if err != nil {
-			pErr := fmt.Errorf("error parsing backup schedule %v: %w", b.schedule, err)
-			klog.Error(pErr)
-			return pErr
-		}
-
-		err = b.scheduleBackup(cCtx, schedule)
+		err := b.scheduleBackup(cCtx, b.cronSchedule)
 		if err != nil {
 			return err
 		}
@@ -113,11 +110,12 @@ func (b *backupServer) scheduleBackup(ctx context.Context, schedule cron.Schedul
 		case <-ticker.C:
 			err := b.runBackup()
 			if err != nil {
+				klog.Errorf("error running backup: %v", err)
 				return err
 			}
 			ticker.Reset(time.Until(schedule.Next(time.Now())))
 		case <-ctx.Done():
-			klog.Infof("context deadline has exceeded")
+			return nil
 		}
 	}
 }
