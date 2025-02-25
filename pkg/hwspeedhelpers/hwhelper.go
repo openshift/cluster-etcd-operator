013bc953bbf472d500f644ce3d61ddbc574abef0
Bryce Palmer <bpalmer@redhat.com>
2025-02-25 16:45:21 -0500

    wip: test a long election timeout on component regressions
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- pkg/hwspeedhelpers/hwhelper.go
+++ pkg/hwspeedhelpers/hwhelper.go
@@ -20,14 +20,14 @@ func HardwareSpeedToEnvMap(speed operatorv1.ControlPlaneHardwareSpeed) (envs map
 
 func StandardHardwareSpeed() map[string]string {
 	return map[string]string{
-		"ETCD_HEARTBEAT_INTERVAL": "100",
-		"ETCD_ELECTION_TIMEOUT":   "1000",
+		"ETCD_HEARTBEAT_INTERVAL": "1000",
+		"ETCD_ELECTION_TIMEOUT":   "9500",
 	}
 }
 
 func SlowerHardwareSpeed() map[string]string {
 	return map[string]string{
-		"ETCD_HEARTBEAT_INTERVAL": "500",
-		"ETCD_ELECTION_TIMEOUT":   "2500",
+		"ETCD_HEARTBEAT_INTERVAL": "1000",
+		"ETCD_ELECTION_TIMEOUT":   "9500",
 	}
 }
