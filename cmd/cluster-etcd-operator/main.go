722aebbcdc189bf0674637f90f76543f3cb55b80
Joel Speed <joel.speed@hotmail.co.uk>
2026-01-19 09:39:49 +0000

    Revert "Merge pull request #1326 from tjungblu/ETCD-652"
    This reverts commit fde15811e0a41ec0754397282937942b3f893645, reversing
    changes made to 60e6acf0c59380118505ba46e247ad82ca86b0c5.

--- cmd/cluster-etcd-operator/main.go
+++ cmd/cluster-etcd-operator/main.go
@@ -17,7 +17,6 @@ import (
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/readyz"
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/render"
 	requestbackup "github.com/openshift/cluster-etcd-operator/pkg/cmd/request-backup"
-	"github.com/openshift/cluster-etcd-operator/pkg/cmd/rev"
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/verify"
 	"github.com/openshift/cluster-etcd-operator/pkg/cmd/waitforceo"
 	"github.com/openshift/cluster-etcd-operator/pkg/operator"
@@ -76,7 +75,6 @@ func NewSSCSCommand(ctx context.Context) *cobra.Command {
 	cmd.AddCommand(readyz.NewReadyzCommand())
 	cmd.AddCommand(prune_backups.NewPruneCommand())
 	cmd.AddCommand(requestbackup.NewRequestBackupCommand(ctx))
-	cmd.AddCommand(rev.NewRevCommand(ctx))
 
 	return cmd
 }
